774766dc935edf9f952755d904a0e23edc509848
Venom-Py <45040352+enomarozi@users.noreply.github.com>
2020-01-12 22:58:31 +0700

    Update Baconian.py

--- Baconian.py
+++ Baconian.py
@@ -11,9 +11,15 @@ def encrypt():
 def decrypt():
     text = "AABAAABBABABBBAABBAAAAAAABAAABABBBABBAABABAAA"
     result = [text[:i][-5:] for i in range(5,len(text)+5,5)]
-    result_all = ""
+    result_allIJUV = ""
+    result_allDistric = ""
+    for i in result:
+        for j in baconIJUV.keys():
+            if i == baconIJUV[j]:
+                result_allIJUV += j
     for i in result:
         for j in baconDist.keys():
             if i == baconDist[j]:
-                result_all += j
-    print(result_all)             
+                result_allDistric += j
+    print("baconIJUV --> "+result_allIJUV)
+    print("baconDistric --> "+result_allDistric)           
